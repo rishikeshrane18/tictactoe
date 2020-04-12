3ecc2c99e74b2086711e3c213b5665e6cb24aecd
rushi <rushikeshrane1@gmail.com>
2020-04-12 21:39:40 +0530

    tictactoe app

--- app/src/main/java/com/example/gameconnector/MainActivity.java
+++ app/src/main/java/com/example/gameconnector/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.gameconnector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-        public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
             //0= yellow and //1= red
             int activePlayer = 0;
             boolean gameIsActive = true;
-            // 2 mean unplayed
-            int [] gameState = {2,2,2,2,2,2,2,2,2};
+
+            LinearLayout popUp;
+            EditText winningmsg;
+
+            int [] gameState = {2,2,2,2,2,2,2,2,2};          // 2 mean unplayed
             int [][] winningPositions ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
             public void dropIn(View view){
+                GridLayout gridLayout = findViewById(R.id.gridLayout);
+                popUp = findViewById(R.id.popUp);
                 ImageView counter = (ImageView)view;
                 int tappedCounter = Integer.parseInt(counter.getTag().toString());
                 if ( gameState[tappedCounter] ==2 && gameIsActive){
@@ -30,6 +38,7 @@ import android.widget.ImageView;
                         activePlayer = 0;
                     }
                     counter.animate().translationYBy(1000f).setDuration(1000);
+
                     for (int[] winningPosition : winningPositions){
                         if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
@@ -40,41 +49,41 @@ import android.widget.ImageView;
                                 winner = "Yellow";
                             }
 
-                            EditText winningmsg = findViewById(R.id.wonMessage);
-                            winningmsg.setVisibility(View.VISIBLE);
+                            popUp.setVisibility(View.VISIBLE);
+                            winningmsg = findViewById(R.id.wonMessage);
                             winningmsg.setText(winner+" has won!");
-                            Button playagain = findViewById(R.id.playAgainButton);
-                            playagain.setVisibility(View.VISIBLE);
 
                         }else {
                             boolean gameIsOver = true;
                             for (int counterState : gameState){
-                                if( counterState==2) gameIsOver = false;
+                                if( counterState==2){
+                                    gameIsOver = false;
+                                    break;
+                                }
                             }
                             if (gameIsOver){
-                                EditText winningmsg = findViewById(R.id.wonMessage);
-                                winningmsg.setVisibility(View.VISIBLE);
-                                winningmsg.setText("its a draw");
-                                Button playagain = findViewById(R.id.playAgainButton);
-                                playagain.setVisibility(View.VISIBLE);
+                                popUp.setVisibility(View.VISIBLE);
+                                winningmsg.setText("It's a Draw!");
+                                gridLayout.setVisibility(View.INVISIBLE);
                             }
                         }
                     }
                 }
             }
+
             public void PlayAgain (View view) {
                 gameIsActive = true;
-                EditText winningmsg = findViewById(R.id.wonMessage);
-                winningmsg.setVisibility(View.INVISIBLE);
                 activePlayer = 0;
-                for(int i = 0; i < gameState.length;i++){
+                for(int i = 0; i < 9;i++){
                     gameState[i]=2;
-
                 }
-                GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+                GridLayout gridLayout = findViewById(R.id.gridLayout);
                 for (int i=0;i<gridLayout.getChildCount();i++){
                     ((ImageView)gridLayout.getChildAt(i)).setImageResource(0);
                 }
+                popUp = findViewById(R.id.popUp);
+                popUp.setVisibility(View.INVISIBLE);
+                gridLayout.setVisibility(View.VISIBLE);
             }
 
         @Override
